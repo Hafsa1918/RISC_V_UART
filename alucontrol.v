5fc26582139a3e70d488a79900424182a81b143f
Hafsa1918 <70684075+Hafsa1918@users.noreply.github.com>
2024-10-01 09:54:30 +0500

    Update alucontrol.v

--- alucontrol.v
+++ alucontrol.v
@@ -8,14 +8,20 @@ wire [13:0] control;
   always @(*)
 begin 
 case(aluop)
-2'b00: operation=3'b000;
-2'b01: operation=3'b001;
+2'b00: operation=3'b000;											//Add
+2'b01: begin
+		 case (funct3)
+		 3'b000: operation=3'b001;									//Sub
+		 3'b001: operation=3'b100;									//not equal
+		 default: operation = 3'bxxx;
+		 endcase
+		 end
 2'b10: begin
        case(funct3)
-       3'b000:operation=funct7? 3'b001 : 3'b000;
-       3'b111:operation=3'b010;
-       3'b110:operation=3'b011;
-       3'b010:operation=3'b101;
+       3'b000:operation=funct7? 3'b001 : 3'b000;			
+       3'b111:operation=3'b010;									//and
+       3'b110:operation=3'b011;									//or	
+       3'b010:operation=3'b101;									//slt
        default: operation = 3'bxxx;
        endcase
        end
